e6013f353a69395e48170a342caf629d86b48e0f
jaeheetest <remedies1808@gmail.com>
2024-11-26 10:59:12 +0900

    백엔드 리프레시 토큰 생성, 유저 엔티티에 핸드폰 번호 등록, 테스트 완료

--- backend/src/main/java/org/example/backend/dto/UsersDto.java
+++ backend/src/main/java/org/example/backend/dto/UsersDto.java
@@ -29,6 +29,8 @@ public class UsersDto {
     private LocalDateTime createdAt;
     // 성별(남/여)
     private String gender;
+    // 핸드폰번호
+    private String phoneNumber;
     // 주소
     private String address;
     // 태어난 날 (나이를 받으려고했으나, 시간이 지남에따라 자동 계산해야하므로 변경)
@@ -44,7 +46,8 @@ public class UsersDto {
     // 비밀번호 찾기 답변
     private String passwordAnswer;
 
-    public UsersDto(String name, String email, String password, String gender, String address, LocalDate birthDate) {
+    public UsersDto(String name, String email, String password, String gender, String address, LocalDate birthDate
+            ,String passwordQuestion, String passwordAnswer, String phoneNumber) {
         this.name = name;
         this.email = email;
         this.password = password;
@@ -52,6 +55,9 @@ public class UsersDto {
         this.gender = gender;
         this.address = address;
         this.birthDate = birthDate;
+        this.passwordQuestion = passwordQuestion;
+        this.passwordAnswer = passwordAnswer;
+        this.phoneNumber = phoneNumber;
         this.userGrade = 0;
         this.totalPurchaseCount = 0L;
         this.totalPurchasePrice = 0L;
