2620ed839bd8bd9f93e941b2e87dbf374bd50563
remedie-s <tree1808@gmail.com>
2024-11-28 12:46:02 +0900

    Hotfix: 인증정보 로딩중 오류 백엔드만 수정함 추후 ERP도 수정

--- Erp/src/main/java/org/example/erp/controller/EmployeeController.java
+++ Erp/src/main/java/org/example/erp/controller/EmployeeController.java
@@ -7,10 +7,9 @@ import org.example.erp.dto.EmployeeDto;
 import org.example.erp.entity.Employee;
 import org.example.erp.repository.EmployeeRepository;
 import org.example.erp.service.EmployeeService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,25 +23,43 @@ public class EmployeeController {
 
     // 직원 리스트조회
     @GetMapping("/list")
-    public List<EmployeeDto> getEmployee() {
+    public ResponseEntity<?> getEmployee() {
         List<EmployeeDto> allToEmployee = this.employeeService.findAllToEmployee();
-        return allToEmployee;
+        return ResponseEntity.ok(allToEmployee);
     }
     // 직원 부서별 리스트 조회
 
     // 직원 리스트조회(임원, 인사과)
     @GetMapping("/list/ex")
-    public List<EmployeeDto> getEmployeeByEX(@AuthenticationPrincipal Employee user ) {
+    public ResponseEntity<?> getEmployeeByEX(@AuthenticationPrincipal Employee employee ) {
         //TODO 권한 체크 메소드 만들어야함
-        log.info("{}",user);
-        return this.employeeService.findAll();
+        if(employee==null){
+            log.info("{}",employee);
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(this.employeeService.findAll());
     }
 
     // 직원 상세페이지
+    @GetMapping("/detail/{id}")
+    public ResponseEntity<?> getEmployeeDetail(@PathVariable("id") Long id) {
+        EmployeeDto byIdToEmployee = this.employeeService.findByIdToEmployee(id);
+        return ResponseEntity.ok(byIdToEmployee);
+    }
 
     // 직원 상세페이지(임원, 인사과)
+    @GetMapping("/admin/{id}")
+    public ResponseEntity<?> getAdminDetail(@PathVariable("id") Long id) {
+        EmployeeDto byId = this.employeeService.findById(id);
+        return ResponseEntity.ok(byId);
+    }
 
     // 직원 변경
+    @PostMapping("/modify/{id}")
+    public ResponseEntity<?> modifyEmployee(@PathVariable("id") Long id, @RequestBody EmployeeDto employeeDto) {
+        return null;
+    }
+
 
     // 추가로직
 
