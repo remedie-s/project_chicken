b2a6d1f833bf6c7e855713cd16646cfd9c9af2d7
remedie-s <tree1808@gmail.com>
2024-11-29 12:26:22 +0900

    Feat: Erp 프론트 엔드 틀 작성

---
+++ toolpad/app/(dashboard)/login/page.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+
+
+export default function LoginPage(): JSX.Element {
+  
+
+  return (
+    <Typography>
+      Welcome to the Toolpad Login!
+    </Typography>
+  );
+}
