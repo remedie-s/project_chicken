81d9ed50cbf607fb59971455a3e50b5f9af32c0f
remedie-s <tree1808@gmail.com>
2024-11-30 14:41:10 +0900

    Feat: 출퇴근 로직 구현중

--- Erp/src/main/java/org/example/erp/repository/LeaveRepository.java
+++ Erp/src/main/java/org/example/erp/repository/LeaveRepository.java
@@ -3,9 +3,12 @@ package org.example.erp.repository;
 import org.example.erp.entity.Leave;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface LeaveRepository extends JpaRepository<Leave, Long> {
 
     List<Leave> findByEmployee_Id(Long employeeId);
+
+    List<Leave> findByEmployeeIdAndStartDateBetween(Long employeeId, LocalDate startOfMonth, LocalDate endOfMonth);
 }
--- toolpad/app/(dashboard)/finances/page.tsx
+++ toolpad/app/(dashboard)/finances/page.tsx
@@ -3,6 +3,17 @@ import Typography from '@mui/material/Typography';
 
 /**
  * 거래처 목록 불러와서 뿌려줌 , 변경 버튼, 삭제 버튼
+ *     id: number;
+ *     name: string;
+ *     email: string;
+ *     managerName: string;
+ *     phone: string;
+ *     address: string;
+ *     website: string;
+ *     description: string;
+ *     outstanding: number;
+ *     contactStart: string;
+ *     contactEnd: string;
  * 신규 거래처 등록 버튼(링크),
  * @constructor
  */
