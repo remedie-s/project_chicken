ee7804875a01219ab23177c555c2b80b139dc972
remedie-s <tree1808@gmail.com>
2024-12-12 09:32:37 +0900

    Hotfix: 쇼핑몰 프론트엔드 빌드 문제 수정(더미파일 삭제)

--- frontend/src/app/product/search/[id]/page.tsx
+++ frontend/src/app/product/search/[id]/page.tsx
@@ -1,12 +1,6 @@
 
 "use client"
 import {Box} from "@mui/material";
-import QuestionDetail from "@/components/userQA/QuestionDetail";
-import useAuth from "@/scripts/auth/useAuth";
-import {useEffect, useState} from "react";
-import authApi from "@/scripts/auth/authApi";
-import {QuestionDto} from "@/types/questionType";
-
 export default function page(){
 
 
