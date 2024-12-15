7fdf4fe7e56476cdf15b64e03b3e8d5d500e72c7
remedie-s <tree1808@gmail.com>
2024-12-15 19:23:33 +0900

    Feat: 개인 채팅 구현 완료

--- Erp/src/main/java/org/example/erp/controller/ChatRestController.java
+++ Erp/src/main/java/org/example/erp/controller/ChatRestController.java
@@ -1,20 +1,24 @@
 package org.example.erp.controller;
 
+import com.google.firebase.messaging.FirebaseMessagingException;
+import lombok.RequiredArgsConstructor;
 import org.example.erp.entity.ChatMessageEntity;
+import org.example.erp.entity.Employee;
 import org.example.erp.service.ChatService;
+import org.example.erp.service.EmployeeService;
+import org.example.erp.service.FirebaseService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/chats")
 public class ChatRestController {
 
     private final ChatService chatService;
+    private final EmployeeService employeeService;
+    private final FirebaseService firebaseService;
 
-    public ChatRestController(ChatService chatService) {
-        this.chatService = chatService;
-    }
 
     // 1:1 채팅 내역 조회
     @GetMapping("/private/{user1}/{user2}")
@@ -30,7 +34,7 @@ public class ChatRestController {
 
     // 메시지 저장 (전체 채팅 또는 1:1 채팅)
     @PostMapping
-    public ChatMessageEntity saveMessage(@RequestBody ChatMessageEntity message) {
+    public ChatMessageEntity saveMessage(@RequestBody ChatMessageEntity message) throws FirebaseMessagingException {
         // 메시지의 receiver 필드를 확인하여 전체 채팅인지 개인 채팅인지 구분
         if (message.getReceiver() == null || "all".equalsIgnoreCase(message.getReceiver())) {
             // 전체 채팅
@@ -39,6 +43,16 @@ public class ChatRestController {
             // 1:1 채팅
             if (message.getSender() == null || message.getReceiver() == null) {
                 throw new IllegalArgumentException("1:1 채팅의 경우 sender와 receiver가 반드시 필요합니다.");
+            }
+        }
+        if(!message.getReceiver().equals("all")) {
+            Employee byEmail = this.employeeService.findByEmail(message.getReceiver());
+            if (byEmail != null) {
+                this.firebaseService.sendPushNotificationUser(
+                        byEmail.getId(),
+                        "새로운 메시지가 도착했습니다.",
+                        message.getSender() + "님이 당신에게 메시지를 보냈습니다."
+                );
             }
         }
 
