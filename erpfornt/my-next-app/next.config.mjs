25f448c00090ad2fa8c9fc1b0cddf4a5ceb1d218
remedie-s <tree1808@gmail.com>
2024-12-19 10:34:59 +0900

    Feat: Https 테스트입니다.

--- erpfornt/my-next-app/next.config.mjs
+++ erpfornt/my-next-app/next.config.mjs
@@ -4,8 +4,8 @@ const nextConfig = {
 
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || (process.env.NODE_ENV === 'production' ? 'http://192.168.0.11:8081/api' : 'http://localhost:8081/api'),
-    NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL || (process.env.NODE_ENV === 'production' ? 'http://192.168.0.11:8081/ws' : 'http://localhost:8081/ws'),
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || (process.env.NODE_ENV === 'production' ? 'https://192.168.0.11:8081/api' : 'https://localhost:8081/api'),
+    NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL || (process.env.NODE_ENV === 'production' ? 'https://192.168.0.11:8081/ws' : 'https://localhost:8081/ws'),
   },
 };
 
